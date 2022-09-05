ca0021250bb2d3e9caa19f9c383b0fbc6fc48d30
Collins Mutai <collinsfrontend@gmail.com>
2022-09-05 16:05:10 +0300

    added home

--- FrontendSendIT/src/app/app.module.ts
+++ FrontendSendIT/src/app/app.module.ts
@@ -2,13 +2,36 @@ import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
 import { AppComponent } from './app.component';
+import { HeaderComponent } from './header/header.component';
+import { HomeComponent } from './home/home.component';
+import { SignupComponent } from './signup/signup.component';
+import { LoginComponent } from './login/login.component';
+import { AdmindashboardComponent } from './admindashboard/admindashboard.component';
+import { UserdashboardComponent } from './userdashboard/userdashboard.component';
+import { CommonModule } from '@angular/common';
+
+import { LottieModule } from 'ngx-lottie';
+import player from 'lottie-web';
+
+export function playerFactory() {
+  return player;
+}
+
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HeaderComponent,
+    HomeComponent,
+    SignupComponent,
+    LoginComponent,
+    AdmindashboardComponent,
+    UserdashboardComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    CommonModule,
+    [LottieModule.forRoot({ player: playerFactory })],
   ],
   providers: [],
   bootstrap: [AppComponent]
---
+++ FrontendSendIT/src/app/home/home.component.ts
@@ -0,0 +1,24 @@
+import { Component, OnInit } from '@angular/core';
+import { AnimationItem } from 'lottie-web';
+import { AnimationOptions } from 'ngx-lottie';
+
+@Component({
+  selector: 'app-home',
+  templateUrl: './home.component.html',
+  styleUrls: ['./home.component.css']
+})
+export class HomeComponent implements OnInit {
+  constructor() { }
+  
+  ngOnInit(): void { }
+  
+  options: AnimationOptions = {
+    path: '/assets/lottie/deliverymotorbike.json',
+  };
+  
+  animationCreated(animationItem: AnimationItem): void {
+    console.log(animationItem);
+  }
+  
+
+}
