4fdc0c5f055816334a629101d9fe1fee7d6e6079
Collins Mutai <collinsfrontend@gmail.com>
2022-09-07 10:35:16 +0300

    about

--- FrontendSendIT/src/app/admin/create-order/create-order.component.ts
+++ FrontendSendIT/src/app/admin/create-order/create-order.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { FormControl, FormGroup, Validators } from '@angular/forms';
 
 @Component({
   selector: 'app-create-order',
@@ -6,10 +7,34 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./create-order.component.css']
 })
 export class CreateOrderComponent implements OnInit {
-
+  form!:FormGroup
+  senderName!:string
+  receiverName!:string
+  senderEmail!:string
+  receiverEmail!:string
+  origin!:string
+  destination!:string
+  weight!:string
+  price=0
   constructor() { }
 
   ngOnInit(): void {
-  }
+    this.form = new FormGroup({
+      senderName: new FormControl(null, [Validators.required]),
+      receiverName: new FormControl(null, [Validators.required]),
+      SenderEmail: new FormControl(null, [Validators.required, Validators.email]),
+      ReceiverEmail: new FormControl(null, [Validators.required, Validators.email]),
+      origin: new FormControl(null, [Validators.required]),
+      destination: new FormControl(null, [Validators.required]),
+      dispatchedDate: new FormControl(null, [Validators.required]),
+      deliveryDate: new FormControl(null, [Validators.required]),
+      weight: new FormControl(null, [Validators.required]),
+      price: new FormControl(null, [Validators.required]),
 
+    });
+  }
+onSubmit(){
+  console.log(this.form.value);
+  
+}
 }
